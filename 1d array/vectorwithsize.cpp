51b34a5fe26067980089d1c3b2d487abf32647da
souravpan140 <souravpansourav1@gmail.com>
2025-12-18 22:46:48 +0530

    Create vectorwithsize.cpp

---
+++ 1d array/vectorwithsize.cpp
@@ -0,0 +1,12 @@
+
+
+#include<iostream>
+#include<vector>
+using namespace  std;
+int main(){
+    vector<int>v(5,7);//size 5,each element is 7
+    cout<<v[2];
+    cout<<v[4];
+    cout<<v[3];
+ return 0;
+}
