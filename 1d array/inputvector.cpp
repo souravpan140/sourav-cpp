1e252085f19c25ea73d1fc825829c146d52c0bd8
souravpan140 <souravpansourav1@gmail.com>
2025-12-18 22:44:10 +0530

    Create inputvector.cpp

---
+++ 1d array/inputvector.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<vector>
+using namespace  std;
+int main(){
+    int n;
+    cout<<"Enter the value:";
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<v.size();i++){
+        cin>>v[i];
+    }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i];
+    }
+    return 0;
+}
